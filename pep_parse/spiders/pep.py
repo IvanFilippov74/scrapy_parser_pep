73b7147960e1af49adbf972b56b5a62ab3f498c8
Ivan Filippov <chelyabinezzz@gmail.com>
2023-10-07 08:02:16 +0500

    PEP8

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -10,7 +10,9 @@ class PepSpider(scrapy.Spider):
 
     def parse(self, response):
         pep_table = response.css('section#numerical-index')
-        pep_hrefs = pep_table.xpath('.//a[@class="pep reference internal"]/@href')
+        pep_hrefs = pep_table.xpath(
+            './/a[@class="pep reference internal"]/@href'
+        )
         for link in pep_hrefs:
             yield response.follow(link, callback=self.parse_pep)
 
